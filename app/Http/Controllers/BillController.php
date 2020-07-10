ae2d07e674d8f77a11d86c4204e93db95d33cf1a
Shahid Shafiq <shahid_shafiq@elixir.com>
2020-07-10 16:16:12 +0500

    Update

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Bill;
+use Twilio\Rest\Client;
 
 class BillController extends Controller
 {
@@ -21,9 +22,39 @@ class BillController extends Controller
       }
   }
 
+  public function sendSMS($recipient, $message) {
+    $account_sid = getenv("TWILIO_SID");
+    $auth_token = getenv("TWILIO_AUTH_TOKEN");
+    $twilio_number = getenv("TWILIO_NUMBER");
+    $client = new Client($account_sid, $auth_token);
+    //$recipient = "+923215176024";
+    $client->messages->create($recipient, 
+            ['from' => $twilio_number,
+            'body' => $message] );
+  }
+
+  public function notifyBillDelivery($bill) {
+    $client = $bill->delivery->client;
+    $pay = "";
+    if ($bill->payment == 'cash') {
+      $pay = sprintf("An amount of %.2f has been received.", $bill->amount);
+    } else {
+      $pay = sprintf("A bill amount of %.2f has been generated.", $bill->amount);
+    }
+    
+    $message = sprintf(
+      "Dear %s, Watel delivered %d bottles at your place. %s. Thank you for using our service", 
+      $client->name, $bill->count, $pay);
+
+    $this->sendSMS($client->mobile, $message);
+  }
+
   public function store(Request $request) {
       $bill = new Bill($request->all());
       $bill->save();
+
+      $this->notifyBillDelivery($bill);
+
       return response()->json([
           "message" => "Bill added successfully",
           "bill_id" => $bill->id
